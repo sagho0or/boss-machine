14db496469169ed295d9efab626c3f4a3c444065
unknown <saghar.fadaei@gmail.com>
2025-02-13 14:36:52 +0000

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-'use strict';
-
-const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+const path = require('path');
 
 module.exports = {
-  entry: './browser/index.js',
+  entry: './src/index.js', 
   output: {
-    path: __dirname,
-    filename: './public/js/bundle.js',
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js',
   },
-  context: __dirname,
-  devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
-        }
-      }
-    ]
-  }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  mode: 'development',
 };
