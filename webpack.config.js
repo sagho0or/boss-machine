9203f8af2eb0eb5a887cfffa7dc80414ab2ca53e
unknown <saghar.fadaei@gmail.com>
2025-02-13 14:48:15 +0000

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
+'use strict';
+
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', 
+  entry: './browser/index.js', // مسیر فایل ورودی اصلی پروژه
   output: {
-    path: path.resolve(__dirname, 'public/js'),
+    path: path.resolve(__dirname, 'public/js'), // مسیر خروجی فایل باندل شده
     filename: 'bundle.js',
   },
+  context: __dirname,
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.jsx?$/, // فایل‌هایی که باید پردازش شوند (جاوااسکریپت و JSX)
+        exclude: /(node_modules|bower_components)/, // فایل‌هایی که باید نادیده گرفته شوند
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // استفاده از babel-loader برای پردازش
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'], // تنظیمات Babel برای React و ES6+
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx'], // پسوندهای فایل‌هایی که باید Resolve شوند
   },
-  devtool: 'source-map',
-  mode: 'development',
 };
